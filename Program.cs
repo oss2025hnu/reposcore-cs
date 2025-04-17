fddb55c09d65d839cc551860ac7dbc69459d68d3
song hyun woo <thdgusdn118@naver.com>
2025-04-18 01:07:43 +0900

    [DOC] 옵션 이름 명시 및 자동 도움말 생성 기능 추가
    [DOC] 옵션 이름 명시 및 자동 도움말 생성 기능 추가

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿using Cocona;
 
-CoconaApp.Run(([Argument] string[] repository, bool verbose) => {
-    Console.WriteLine($"Repository: {String.Join("\n ", repository)}");
-    if (verbose) {
-        Console.WriteLine("Verbose mode is enabled.");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CoconaApp.Run(Commands>(args);
     }
-});
+}
+
+public class Commands
+{
+    public void Run(
+        [Argument] string[] repository,
+        [Option("vervose",'v', Description = "자세한 로그 출력을 활성화합니다.")]
+    {
+        Console.WriteLine($"Repository: {string.Join("\n",repository)}");
+
+        if (verbose)
+        {
+            Console.WriteLine("Verbose mode is enabled.");
+        }
+    }
+}
