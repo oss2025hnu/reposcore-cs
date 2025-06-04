2aaf8a8536df2fc1bb649ec41b47e808caba263a
eunwoooo <tabcun88@naver.com>
2025-06-04 17:05:36 +0000

    UserActivity record 생성자 및 초기화 방식 수정

--- Reposcore/CommonData.cs
+++ Reposcore/CommonData.cs
@@ -8,47 +8,36 @@ public record UserActivity
     public int PR_typo { get; set; }
     public int IS_fb { get; set; }
     public int IS_doc { get; set; }
-
-    // Deconstruct 메서드 추가
-    public void Deconstruct(out int prFb, out int prDoc, out int prTypo, out int isFb, out int isDoc)
-    {
-        prFb = PR_fb;
-        prDoc = PR_doc;
-        prTypo = PR_typo;
-        isFb = IS_fb;
-        isDoc = IS_doc;
-    }
 }
 
+// UserActivity를 분석해서 사용자별 점수를 계산하는 레코드
+public record UserScore(
+    int PR_fb,
+    int PR_doc,
+    int PR_typo,
+    int IS_fb,
+    int IS_doc,
+    int total
+);
 
-
-    // UserActivity를 분석해서 사용자별 점수를 계산하는 레코드
-    public record UserScore(
-        int PR_fb,
-        int PR_doc,
-        int PR_typo,
-        int IS_fb,
-        int IS_doc,
-        int total
-    );
-
-// 1번 단계를 책임지는 Repscore/RepoDataCollector.cs의 클래스의 객체 하나가
-// 모아오는 데이타가 바로 repo1Activities 같은 것이다.
 public static class DummyData
 {
     public static Dictionary<string, UserActivity> repo1Activities = new()
     {
-        { "user00", new UserActivity { PR_fb = 1, PR_doc = 0, PR_typo = 0, IS_fb = 0, IS_doc = 0 } },
-        { "user01", new UserActivity { PR_fb = 0, PR_doc = 1, PR_typo = 0, IS_fb = 0, IS_doc = 0 } },
-        { "user02", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 1, IS_fb = 0, IS_doc = 0 } },
-        { "user03", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 0, IS_fb = 1, IS_doc = 0 } },
-        { "user04", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 0, IS_fb = 0, IS_doc = 1 } },
-        { "user05", new UserActivity { PR_fb = 10, PR_doc = 0, PR_typo = 0, IS_fb = 0, IS_doc = 0 } },
-        { "user06", new UserActivity { PR_fb = 0, PR_doc = 10, PR_typo = 0, IS_fb = 0, IS_doc = 0 } },
-        { "user07", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 10, IS_fb = 0, IS_doc = 0 } },
-        { "user08", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 0, IS_fb = 10, IS_doc = 0 } },
-        { "user09", new UserActivity { PR_fb = 0, PR_doc = 0, PR_typo = 0, IS_fb = 0, IS_doc = 10 } },
+        { "user00", new UserActivity { PR_fb = 1 } },
+        { "user01", new UserActivity { PR_doc = 1 } },
+        { "user02", new UserActivity { PR_typo = 1 } },
+        { "user03", new UserActivity { IS_fb = 1 } },
+        { "user04", new UserActivity { IS_doc = 1 } },
+        { "user05", new UserActivity { PR_fb = 10 } },
+        { "user06", new UserActivity { PR_doc = 10 } },
+        { "user07", new UserActivity { PR_typo = 10 } },
+        { "user08", new UserActivity { IS_fb = 10 } },
+        { "user09", new UserActivity { IS_doc = 10 } },
     };
+
+    // 1번 단계를 책임지는 Repscore/RepoDataCollector.cs의 클래스의 객체 하나가
+    // 모아오는 데이타가 바로 repo1Activities 같은 것이다.
 
     public static Dictionary<string, UserActivity> repo2Activities = new()
     {
@@ -61,6 +50,21 @@ public static class DummyData
         { "user10", new UserActivity { PR_fb = 21, PR_doc = 9, PR_typo = 13, IS_fb = 4, IS_doc = 11 } },
         { "user11", new UserActivity { PR_fb = 2, PR_doc = 18, PR_typo = 7, IS_fb = 15, IS_doc = 10 } },
         { "user12", new UserActivity { PR_fb = 16, PR_doc = 3, PR_typo = 12, IS_fb = 8, IS_doc = 14 } },
+    };
+
+    public static Dictionary<string, UserScore> repo1Scores = new()
+    {
+        {"user01", new UserScore(21, 8, 0, 4, 3, 36)},
+        {"user02", new UserScore(12, 6, 5, 2, 1, 26)},
+        {"user03", new UserScore(3, 2, 3, 6, 2, 16)},
+        {"user04", new UserScore(18, 10, 4, 8, 1, 41)},
+        {"user05", new UserScore(9, 4, 2, 2, 5, 22)},
+        {"user06", new UserScore(6, 12, 1, 6, 3, 28)},
+        {"user07", new UserScore(15, 14, 5, 4, 2, 40)},
+        {"user08", new UserScore(27, 16, 3, 10, 4, 60)},
+        {"user09", new UserScore(30, 6, 0, 12, 1, 49)},
+        {"user10", new UserScore(24, 18, 2, 14, 2, 60)},
+        {"user11", new UserScore(33, 20, 4, 16, 5, 78)}
     };
 }
 
